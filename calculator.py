759cfa9b247f7a42124d6f9726a0f86fade67ceb
voberl <vinzentoberl@gmail.com>
2026-03-27 10:32:05 +0100

    Add subtraction function

--- calculator.py
+++ calculator.py
@@ -4,3 +4,17 @@ def add(a, b):
 
 if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
+
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
+    print("5 - 3 =", sub(5, 3))
